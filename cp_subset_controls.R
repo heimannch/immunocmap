211abb1c0bc76a4a76df9f1aadd1051368f654d7
Carol Heimann <cheimann@Avocado.systemsbiology.net>
2019-05-31 17:09:00 -0700

    improved documentation

--- cp_subset_controls.R
+++ cp_subset_controls.R
@@ -10,7 +10,10 @@ library(skimr)
 library(plyr)
 library(dplyr)
 
-# 1. Selection of controls for analysis of COMPOUNDS!
+
+
+# 1. Selection of controls for analysis of compounds
+
 # select consensus controls: "ctl_vehicle.cns" and "ctl_trt.cns"
 
 sig_ctl <- siginfo %>% filter(stringr::str_detect(pert_type,"ctl_") & str_detect(pert_type,".cns"))
@@ -46,6 +49,13 @@ sigs <- subset(siginfo, pert_type == "trt_cp" & cell_id %in% list_ctl$cell_id & 
 #3. Threshold of "modulating z-score" for each cell line and controls
 #given a cell line, extract the associated consensus controls, and determine the maximum z-score for a given quantile
 
+#' Computes the 1% and 99% quantile of the distribution of expression data for a gene for each experiment involving a control.
+#' 
+#' @param cell cell_id to compute the threshold
+#' @param list_ctls List of signatures id associated with controls.
+#' @param pert The type of control to be used. Options are "DMSO", "UnTrt", "H2O". The default uses "DMSO" and "UnTrt". This ignores controls with H2O, because they consistently have different results, and are available only for a few cell lines) 
+#' @return Dataframe with the 1% and 99% quantile of each selected control 
+#' 
 get_thresholds <- function(cell = "MCF7", list_ctls = list_ctl, pert = FALSE){
   #in case the threshold should be computed using only one kind of control, inform name (options: "DMSO", "UnTrt", "H2O")
   #The default ignores controls with H2O (consistently different results, and available only for a few cell lines)  
@@ -88,10 +98,19 @@ get_z_threshold <- function(cell_id, list_ctls = list_ctl, pert = FALSE){
   return(skim_to_wide(zs)) 
 }
 
-#Creating a list of the summary statistics of the 1% and 99% quantiles of z-scores of controls for a given cell line
+
+#' Computes the summary statistics of the 1% and 99% quantile of the distribution of expression data for all controls for a cell line.
+#' 
+#' @param list_cells cell_id or vector of cell_id to compute the threshold
+#' @param tissue Set to false when searching based on cell_id. Otherwise, select here the name of the tissue of interest
+#' @param list_ctls List of signatures id associated with controls.
+#' @param pert The type of control to be used. Options are "DMSO", "UnTrt", "H2O". The default uses "DMSO" and "UnTrt". This ignores controls with H2O, because they consistently have different results, and are available only for a few cell lines) 
+#' @return Dataframe with the 1% and 99% quantile of each selected control 
+#' @examples
+#' get_z_list(c("MCF7", "A375"), tissue = FALSE)
+#' get_z_list(list_cells = FALSE, tissue = "breast")
+
 get_z_list <- function(list_cells = "MCF7", tissue = FALSE, list_ctls = list_ctl, pert = FALSE){
-#by default, it is returned a list with all cell lines with controls.
-#The user needs to input a cell line name, but it can be changed by supplying a primary site name
 
 #Geting the list of cell_id requested  
   if(tissue != FALSE){
@@ -115,6 +134,18 @@ get_z_list <- function(list_cells = "MCF7", tissue = FALSE, list_ctls = list_ctl
 
 #------------------------------------------------------------------------------------------------------------------------------#
 #4. Threshold of Signature Strength for each cell line
+
+#' Computes the Signature Strength of each control signature
+#' The Connectivity Map dataset contains an annotation of how many landmark genes were up and down modulated by each signature. 
+#' This can be considered a proxy of specificity of the action of a perturbation - if the signature strength is high, one could argue that this affects a lot of genes. 
+#' On the other hand, if it is low, there could imply a more specific relationship between the perturbation and the affected genes.
+#' 
+#' @param cells cell_id to select the controls to compute the signature stregth
+#' @param list_ctls List of signatures id associated with controls.
+#' @param pert The type of control to be used. Options are "DMSO", "UnTrt", "H2O". The default uses "DMSO" and "UnTrt". This ignores controls with H2O, because they consistently have different results, and are available only for a few cell lines) 
+#' @return Dataframe with number of landmark genes up and down modulated by each gene signature 
+#' @example 
+#' get_ss_threshold("MCF7")
 
 get_ss_threshold <- function(cell, list_ctls = list_ctl, pert = FALSE){
   
@@ -141,9 +172,6 @@ get_ss_threshold <- function(cell, list_ctls = list_ctl, pert = FALSE){
   
 }
 
-
-# test <- get_ss_threshold(cell = "A375")
-# print(test)
 
 
 
--- gene_modulation.R
+++ gene_modulation.R
@@ -12,9 +12,103 @@ library(tidyr)
 
 
 
-#function to grab signatures with the same pertubagem in two different times
-get_timelapse <- function(time1, time2, n_replicates){
+#' Function to query for expression data in the Connectivity Map dataset with output in tidy format.
+#' 
+#' @param gene_id The id of the gene of interest to query. It must be available from the `geneinfo` dataframe (loaded in session by the `load_data.R` script)  
+#' @param signatures The id of the signatures of interest. It must be available from the `siginfo` dataframe (loaded in session by the `load_data.R` script)
+#' @param n_replicates The minimal number of replicates a signature needs to have to be included in the analysis
+#' @return Dataframe in tidy format with each row containing the signature of the experiment, a gene id and the corresponding z-score
+#' @example 
+#' get_z_score(1493, siginfo[10:30, 1])
 
+get_zscores <- function(gene_id, signatures){
+   
+  #Querying the dataset, based on the gene of interest and the selected signatures
+  ds <- cmapR::parse.gctx(ds_path,
+                          rid=as.character(gene_id),
+                          cid=signatures)
+  
+  tidy_ds <- as.data.frame(ds@mat) %>% mutate(pr_gene_id = row.names(ds@mat)) %>% tidyr::gather(key = sig_id, z_score, - pr_gene_id)
+  
+  return(tidy_ds)
+}
+
+
+#-------Functions to select signatures based on a threshold and cell line-----------------------#
+
+
+#' Create a dataframe of signatures with up, down and non modulated z-scores.
+#' 
+#' @param gene_id The id of the gene of interest to query. It must be available from the `geneinfo` dataframe (loaded in session by the `load_data.R` script)  
+#' @param signatures The id of the signatures of interest. It must be available from the `siginfo` dataframe (loaded in session by the `load_data.R` script)
+#' @param mod_thres The threshold of z-score to consider up or down modulation. Absolute values higher that the threshold are considered modulated, while lower values are not-modulated
+#' @return Dataframe in tidy format with each row containing the signature of the experiment, a gene id, the corresponding z-score, and if the gene is up, down or not modulated
+#' @example 
+#' get_mod_sigs(1493, siginfo[10:30, 1], 1.5)
+#' 
+get_mod_sigs <- function(gene_id, signatures, mod_thres = 2){
+  #mod_thres - z-score used as a threshold to determine if a gene is modulated by the signature - the higher the more modulated. 
+  #The negative value is used to threshold down-modulation.
+  
+  tidy_ds <- get_zscores(gene_id, signatures)
+ 
+  #getting the signatures that significantly modulated the gene, assuming that it is represented by an absolute z-score greater than mod_thres
+  
+  tidy_mod <- tidy_ds %>% mutate(mod = case_when(z_score> mod_thres ~ "up",
+                                                z_score< (-mod_thres) ~ "dn",
+                                                z_score > (-mod_thres) | z_score < mod_thres ~ "no"))
+  
+  return(tidy_mod)
+  
+}
+
+
+#' Create a dataframe of signatures with up, down and non modulated z-scores, indexed by cell line.
+#' 
+#' Each cell line may have a different z-score as threshold criteria for modulation. This function is useful to compute and organize the data considering these differences
+#' 
+#' @param controls_stats Dataframe with the threshold of modulation for each cell line. The column with the threshold information should be named "mean".  
+#' @param list_genes List of genes to query against the Connectivity Map dataset. 
+#' @param signatures The id of the signatures of interest. It must be available from the `siginfo` dataframe (loaded in session by the `load_data.R` script)
+#' @return Dataframe in tidy format with each row containing the signature of the experiment, the cell line, a gene id, the corresponding z-score, and if the gene is up, down or not modulated
+#' 
+
+get_modsigs_per_cellid <- function(controls_stats, list_genes, signatures = siginfo){
+
+  sig_mod <- data.frame()
+  
+  for (i in 1:nrow(controls_stats)){
+
+    cell <- as.character(controls_stats[i,2])
+    threshold <- subset(controls_stats, variable == cell, mean)
+    sigs <- subset(signatures, cell_id == cell, sig_id)
+
+
+    sig_mod <- rbind(sig_mod, get_mod_sigs(gene_id = list_genes, signatures = sigs$sig_id, mod_thres = as.numeric(threshold)))
+    i=i+1
+  }
+  
+  return(sig_mod)
+  
+}
+
+
+
+#---deprecated functions - check if I want to keep them ---#
+
+#' Selects signatures with the same pertubagens in two different time frames. The names of the objects of manipulation are inherited from 'load_data.R' 
+#' This function is useful in case there is interest in comparing the effect of time in gene expression levels and provides an filtering stage based on particular stages of interest.
+#' The CMap dataset have data only for the following times:  1 h 2 h 3 h 4 h 6 h 24 h 48 h 72 h 96 h 120 h 144 h 168 h
+#' 
+#' @param time1 Number representing one of the time frames to be considered in the analysis 
+#' @param time2 Number representing the second of the time frames to be considered in the analysis
+#' @param n_replicates The minimal number of replicates a signature needs to have to be included in the analysis
+#' @return Dataframe with the signatures with pertubations in the two time frames provided 
+#' @example 
+#' get_timelapse(6,24,3)
+
+get_timelapse <- function(time1, time2, n_replicates){
+  
   #adding important columns in the sigmetrics dataset
   index <- match(sigmetrics$sig_id, siginfo$sig_id)
   sigmetrics$cell_id <- siginfo$cell_id[index]
@@ -31,36 +125,6 @@ get_timelapse <- function(time1, time2, n_replicates){
   
 }
 
-#getting a subset of signatures, based on the number of replicates and time of interest
-get_onetime_sig <- function(n_replicates, time){
-  #time must be in number of hours. The CMap dataset have data only for the following times:  1 h 2 h 3 h 4 h 6 h 24 h 48 h 72 h 96 h 120 h 144 h 168 h 
-  
-  #adding important columns in the sigmetrics dataset
-  index <- match(sigmetrics$sig_id, siginfo$sig_id)
-  sigmetrics$cell_id <- siginfo$cell_id[index]
-  sigmetrics$pert_time <- siginfo$pert_time[index]
-  sigmetrics$pert_itime <- siginfo$pert_itime[index]
-  
-  sigs_of_interest <- sigmetrics[distil_nsample>n_replicates & pert_time == time]
-  sig_ids <- sigs_of_interest$sig_id
-  
-  return(sig_ids)
-}
-  
-get_zscores <- function(gene_id, signatures){
-  #gene_id must be the number in the "pr_gene_id" in the "geneinfo" table
-  #Signatures must be a list only with the signatures (indexed by $sig_id in the metadata)
-   
-  #Querying the dataset, based on the gene of interest and the selected signatures
-  ds <- cmapR::parse.gctx(ds_path,
-                          rid=as.character(gene_id),
-                          cid=signatures)
-  
-  tidy_ds <- as.data.frame(ds@mat) %>% mutate(pr_gene_id = row.names(ds@mat)) %>% tidyr::gather(key = sig_id, z_score, - pr_gene_id)
-  
-  return(tidy_ds)
-}
-
 #function returns the cell sample type (normal or tumor) for a list of signatures
 get_cell_sample_types <- function(signatures){
   
@@ -72,7 +136,7 @@ get_cell_sample_types <- function(signatures){
   
   return(list_cells)
 }
-  
+
 #function returns a histogram comparing the distribution of z-scores for a given gene in two different time-frames
 get_hist_zscore <- function(gene_id, signatures, by_cellsample_type = FALSE){. 
   #The signature table needs to contain the list of signatures coded as "sig_id" 
@@ -120,7 +184,7 @@ get_hist_zscore <- function(gene_id, signatures, by_cellsample_type = FALSE){.
   print(hist_plot)
   
 }
-  
+
 
 get_dist_zscore_onetime <- function(gene_id, signatures){
   #function returns a histogram comparing the distribution of z-scores for a given gene, regardless of the time of the experiment. 
@@ -137,47 +201,4 @@ get_dist_zscore_onetime <- function(gene_id, signatures){
   hist(ds@mat, col = "dodgerblue", main = as.character(my_gene), xlab="z-score")
   abline(v=0.54, lty=2, col=2, lwd=1.3 )
   abline(v=-0.54, lty=2, col=2, lwd=1.3 )
-}
-
-
-#-------Functions to select signatures based on a threshold and cell line-----------------------#
-
-#1. Create a list of signatures with up, down and non modulated z-scores.
-
-get_mod_sigs <- function(gene_id, signatures, mod_thres = 2){
-  #mod_thres - z-score used as a threshold to determine if a gene is modulated by the signature - the higher the more modulated. 
-  #The negative value is used to threshold down-modulation.
-  #non_mod_thres - z-score used to determine if a gene is NOT modulated by the signature - "non-regulation" increases when closer to zero
-  
-  tidy_ds <- get_zscores(gene_id, signatures)
- 
-  #getting the signatures that significantly modulated the gene, assuming that it is represented by an absolute z-score greater than mod_thres
-  
-  tidy_mod <- tidy_ds %>% mutate(mod = case_when(z_score> mod_thres ~ "up",
-                                                z_score< (-mod_thres) ~ "dn",
-                                                z_score > (-mod_thres) | z_score < mod_thres ~ "no"))
-  
-  return(tidy_mod)
-  
-}
-
-
-#Returns the list of annotation of up and down modulation, indexed by the cell line
-get_modsigs_per_cellid <- function(controls_stats, list_genes, signatures = siginfo){
-
-  sig_mod <- data.frame()
-  
-  for (i in 1:nrow(controls_stats)){
-
-    cell <- as.character(controls_stats[i,2])
-    threshold <- subset(controls_stats, variable == cell, mean)
-    sigs <- subset(signatures, cell_id == cell, sig_id)
-
-
-    sig_mod <- rbind(sig_mod, get_mod_sigs(gene_id = list_genes, signatures = sigs$sig_id, mod_thres = as.numeric(threshold)))
-    i=i+1
-  }
-  
-  return(sig_mod)
-  
 }
